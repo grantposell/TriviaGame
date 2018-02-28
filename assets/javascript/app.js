76ea9b299710016393144bbfdc50f3d69b17fe0b
Grant Posell <grantposell@Grants-MacBook-Pro.local>
2018-02-27 23:16:21 -0600

    Form and questions formatted

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// First make the timer
+
+//Convert the time from minutes and seconds
+
+//Start the timer a 5 seconds after the page loads
+
+
+
+//When the user submits the form
+
+$('#quiz-form').on("submit", function(event) {
+    event.preventDefault();
+});
+//Chck what answer they picked
+
+//Give them a score 
