65a9e07008691936641e273f9b3c3cc79378e6dd
Grant Posell <grantposell@Grants-MacBook-Pro.local>
2018-02-28 22:36:45 -0600

    Added the timer feature.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,55 @@
-// First make the timer
+$(document).ready(function () {
+    // First make the timer
+    var number = 120;
+    
+    var intervalId;
 
-//Convert the time from minutes and seconds
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+    //  The decrement function.
+    function decrement() {
 
-//Start the timer a 5 seconds after the page loads
+        //  Decrease number by one.
+        number--;
+  
+        //  Show the number in the #show-number tag.
+        $("#show-number").html("<h2>" + number + "</h2>");
+         //  Once number hits zero...
+      if (number === 0) {
 
+        //  ...run the stop function.
+        stop();
 
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
 
-//When the user submits the form
+    }
+    //  The stop function
+    function stop() {
 
-$('#quiz-form').on("submit", function(event) {
-    event.preventDefault();
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+    //  Execute the run function.
+    run();
+    $('#quiz-form').on("submit", function (event) {
+        event.preventDefault();
+    });
+    //Start the timer a 5 seconds after the page loads
 });
-//Chck what answer they picked
 
-//Give them a score 
+    //When the user submits the form
+
+   
+    //Chck what answer they picked
+
+    //Give them a score 
+
+
+
